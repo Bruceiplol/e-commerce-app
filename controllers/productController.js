a244bf9f43c1026a232b9f3cdfd5cfa8f84cb500
Bruceiplol <bruceihizzz@gmail.com>
2024-02-15 15:28:26 +0800

    complete product

---
+++ controllers/productController.js
@@ -0,0 +1,27 @@
+const createError = require('http-errors');
+const productModel = require('../models/productModel');
+const productModelInstance = new productModel();
+
+module.exports = class Product {
+  async get(id) {
+    try {
+      const product = await productModelInstance.findOneById(id)
+
+      if (!product) {
+        throw createError(404, 'Product not found');
+      }
+      return product
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async list(options) {
+    try {
+      const products = await productModelInstance.find(options)
+      return products
+    } catch(err) {
+      throw err
+    }
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const authRouter = require('./auth')
 const userRouter = require('./user')
+const productRouter = require('./product')
 
 module.exports = (app, passport) => {
   authRouter(app, passport)
   userRouter(app)
+  productRouter(app)
 }
